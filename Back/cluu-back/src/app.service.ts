2f6c2f93341966d9ef6ab9ce49283a9be3d47a5d
cluu <lu452126871@gmail.com>
2024-04-07 08:51:46 +1000

    remove back

--- Back/cluu-back/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import mongoose from 'mongoose';
-@Injectable()
-export class AppService {
-  getHello(): string {
-    mongoose.connect
-    return 'Hello World!';
-  }
-}
--- Back/cluu-back/src/user.dto.ts
+++
@@ -1,5 +0,0 @@
-export default class CreateUserDto {
-    readonly name: string;
-    readonly age: number;
-    readonly height?: number;
-  }
