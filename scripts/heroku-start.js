e56a904945d6a3c9392cdcab6cb850983205ca7f
cluu <lu452126871@gmail.com>
2024-04-08 14:38:08 +1000

    add heoku

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,19 +1,29 @@
-// 创建新文件 script/heroku-start.js
-const express = require('express');
-const 路径 = require('路径');
-常量应用程序 = Express();
-const port = process.env.PORT || 3000
-    app.use(express.json());
-// 您的静态预构建资产文件夹
-app.use(express.static(path.join(__dirname, '..', 'build')));
-// 根目录重定向到预构建资源
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build'));
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Check if request is from HTTP and if so, redirect to HTTPS
+app.enable("trust proxy");
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
 });
-// 任何页面重定向到预构建资产文件夹index.html // 将加载react应用程序
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '. .', 'build/index.html'));
+
+app.use(express.json());
+
+// Your static site folder name
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// Root Redirects to the build in assets folder
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "build"));
 });
+
+// Any Page Redirects to the build in assets folder index.html that will load the react app
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "build/index.html"));
+});
+
 app.listen(port, () => {
-    console.log("服务器正在端口上运行：", port)
-})
+    console.log("Server is running on port: ", port);
+});
