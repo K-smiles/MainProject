8da2c11fcea381549b6d9ae18d16e8ba2d304782
cluu <lu452126871@gmail.com>
2024-04-25 11:43:11 +1000

    fixed 3d model path

--- src/pages/model3d/content/OlderModel.js
+++ src/pages/model3d/content/OlderModel.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
-
 export default function Model(props) {
-    const gltf = useGLTF('/model/Animated Full Human Body Anatomy18/Animated Full Human Body Anatomy.gltf')
+    const gltf = useGLTF('/model/HumanBody.gltf')
     return <primitive position={props.position} rotation={[0,Math.PI/6,0]} object={gltf.scene} />
 }
 
-useGLTF.preload('/model/Animated Full Human Body Anatomy18/Animated Full Human Body Anatomy.gltf')
+useGLTF.preload('/model/HumanBody.gltf')
