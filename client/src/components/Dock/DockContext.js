33d9d06d37011809492ce077cb1c448d4838bc35
cluu <lu452126871@gmail.com>
2024-05-14 09:29:50 +1000

    continue delte file

--- client/src/components/Dock/DockContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react'
-
-export const DockContext = createContext({ width: 0, hovered: false, zoomLevel: 1, setIsZooming: () => { } })
-
-export const useDock = () => {
-  return useContext(DockContext)
-}
